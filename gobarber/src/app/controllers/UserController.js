52cf148df43a1687518948b366b88af27d994811
Maria <nathyellemonteiro19@gmail.com>
2020-01-03 11:03:15 -0300

    created auth middleware and user update method

--- gobarber/src/app/controllers/UserController.js
+++ gobarber/src/app/controllers/UserController.js
@@ -17,6 +17,35 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (password && (await user.checkPassword(password))) {
+      return res
+        .status(400)
+        .json({ error: 'New password cannot be equal the old one' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email: user.email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
